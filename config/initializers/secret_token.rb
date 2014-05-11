4b2b3f81c628238d776746eb1ba428660ba624b9
Rebecca Collins <hello@rebeccacollins.me>
2014-05-11 13:33:35 -0400

    first rails tutorial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RebeccasApp::Application.config.secret_key_base = '4de0f83978da0cb58445e7eb926543cab9a231118268948e60ac3ca55ef70e05950656d4e5eefc1e881d4c4a23315401ca4528360bea2be8d5a1edfabd1c2640'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RebeccasApp::Application.config.session_store :cookie_store, key: '_rebeccas_app_session'
